811cfa174371b126fc639781c87787396c6188eb
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-17 10:21:01 +0700

    update project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 
+
+const students = [{
+    company: 'Alfreds Futterkiste', contact: 'Maria Anders', country: 'Germany'
+}, {
+    company: 'Centro comercial Moctezuma', contact: 'Francisco Chang', country: 'Mexico'
+}, {
+    company: 'Ernst Handel', contact: 'Roland Mendel', country: 'Austria'
+}, {
+    company: 'Island Trading', contact: 'Helen Bennett', country: 'UK'
+}, {
+    company: 'Laughing Bacchus Winecellars', contact: 'Yoshi Tannamuri', country: 'Canada'
+}, {
+    company: 'Magazzini Alimentari Riuniti', contact: 'Giovanni Rovelli', country: 'Italy'
+}]
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<div className="container py-5">
+    <table className="table  table-hover table-striped table-sm">
+        <thead>
+        <tr>
+            <th scope='col'>#</th>
+            <th scope='col'>Company</th>
+            <th scope='col'>Contact</th>
+            <th scope='col'>Country</th>
+        </tr>
+        </thead>
+        <tbody>
+        {students.map(student => (<tr>
+            <th scope='row'>{students.indexOf(student) + 1}</th>
+            <td>{student.company}</td>
+            <td>{student.contact}</td>
+            <td>{student.country}</td>
+        </tr>))}
+        </tbody>
+    </table>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+</div>);
